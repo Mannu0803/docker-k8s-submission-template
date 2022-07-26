fa9322d4bac2ebfb3499ca6b3c7d73b50c0ed8c4
Mannu0803 <110046081+Mannu0803@users.noreply.github.com>
2022-07-26 20:31:00 +0530

    Add files via upload

---
+++ manifests/project.js
@@ -0,0 +1,15 @@
+
+const http = require('http');
+
+http.createServer((request, response) => {
+
+   
+        'Content-Type': 'text/plain'
+    });
+
+   
+    response.write('CWiS Assessment\n');
+
+   
+
+}).listen(8000); // 
